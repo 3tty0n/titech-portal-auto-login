983bb2a78a2a425b83ee14911415f5220d35cc6c
Yusuke Izawa <yuizalp@gmail.com>
2017-02-06 01:03:04 +0900

    refactoring

--- titech_portal_auto.rb
+++ titech_portal_auto.rb
@@ -1,46 +1,52 @@
 require 'selenium-webdriver'
 require './env.rb'
 
-driver = Selenium::WebDriver.for :firefox
+@driver = Selenium::WebDriver.for :firefox
 
-driver.get 'https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword'
-usr_name = driver.find_element(:name, 'usr_name')
+@driver.get 'https://portal.nap.gsic.titech.ac.jp/GetAccess/Login?Template=userpass_key&AUTHMETHOD=UserPassword'
+usr_name = @driver.find_element(:name, 'usr_name')
 usr_name.send_keys(@usr_name)
-usr_pass = driver.find_element(:name, 'usr_password')
+usr_pass = @driver.find_element(:name, 'usr_password')
 usr_pass.send_keys(@usr_password)
-driver.find_element(:name, 'OK').click
+@driver.find_element(:name, 'OK').click
 
 sleep 3
 
-def find_element_by_xpath(driver, path)
-  driver.find_element(:xpath, path)
+def find_element_by_xpath(path)
+  @driver.find_element(:xpath, path)
 end
 
-def find_element_by_name(driver, name)
-  driver.find_element(:name, name)
+def find_element_by_name(name)
+  @driver.find_element(:name, name)
 end
 
-def get_password_value(message)
+def get_password_value_from_message(message)
   @usr_matrix[message[0]][message[1] - 1]
 end
 
-def get_message_value(element)
+def get_message_value_from_xpath(element)
   [element.text[1], element.text[3].to_i]
 end
 
-m1 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[5]/th[1]')
-m2 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[6]/th[1]')
-m3 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[7]/th[1]')
+xpath_list = [
+  '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[5]/th[1]',
+  '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[6]/th[1]',
+  '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[7]/th[1]'
+]
 
-message_value = [m1, m2, m3].map { |m| get_message_value(m) }
+pass_list = [
+  find_element_by_name('message3'),
+  find_element_by_name('message4'),
+  find_element_by_name('message5')
+]
 
-p1 = driver.find_element(:name, 'message3')
-p2 = driver.find_element(:name, 'message4')
-p3 = driver.find_element(:name, 'message5')
-
-[p1, p2, p3].zip(message_value).map { |item|
-  p_value = get_password_value(item[1])
-  item[0].send_keys(p_value)
+message_value = xpath_list.map { |e|
+  get_message_value_from_xpath(find_element_by_xpath(e))
 }
 
-driver.find_element(:name, 'OK').click
+pass_list.zip(message_value).map { |pass, message|
+  p_value = get_password_value_from_message(message)
+  pass.send_keys(p_value)
+}
+
+@driver.find_element(:name, 'OK').click
