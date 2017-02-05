d5f1edaf25e3970ba3aac0c4786d99f2a1ebdcec
Yusuke Izawa <yuizalp@gmail.com>
2017-02-05 20:58:17 +0900

    fix titech_portal_auto.rb

--- titech_portal_auto.rb
+++ titech_portal_auto.rb
@@ -20,28 +20,27 @@ def find_element_by_name(driver, name)
   driver.find_element(:name, name)
 end
 
-def get_password_value(usr_matrix, message)
-  usr_matrix[message[0]][message[1] - 1]
+def get_password_value(message)
+  @usr_matrix[message[0]][message[1] - 1]
+end
+
+def get_message_value(element)
+  [element.text[1], element.text[3].to_i]
 end
 
 m1 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[5]/th[1]')
 m2 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[6]/th[1]')
 m3 = find_element_by_xpath(driver, '/html/body/center[3]/form/table/tbody/tr/td/table/tbody/tr[7]/th[1]')
 
-m1_value = [m1.text[1], m1.text[3].to_i]
-m2_value = [m2.text[1], m2.text[3].to_i]
-m3_value = [m3.text[1], m3.text[3].to_i]
+message_value = [m1, m2, m3].map { |m| get_message_value(m) }
 
 p1 = driver.find_element(:name, 'message3')
 p2 = driver.find_element(:name, 'message4')
 p3 = driver.find_element(:name, 'message5')
 
-p1_value = get_password_value(@usr_matrix, m1_value)
-p2_value = get_password_value(@usr_matrix, m2_value)
-p3_value = get_password_value(@usr_matrix, m3_value)
-
-p1.send_keys(p1_value)
-p2.send_keys(p2_value)
-p3.send_keys(p3_value)
+pass_value = [p1, p2, p3].zip(message_value).map { |item|
+  p_value = get_password_value(item[1])
+  item[0].send_keys(p_value)
+}
 
 driver.find_element(:name, 'OK').click
