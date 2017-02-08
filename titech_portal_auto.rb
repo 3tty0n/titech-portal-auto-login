65539b3641fba4d1dda14ed81dd1a17abcc63f03
Yusuke Izawa <yuizalp@gmail.com>
2017-02-08 14:38:49 +0900

    fix sleep time

--- titech_portal_auto.rb
+++ titech_portal_auto.rb
@@ -10,7 +10,7 @@ usr_pass = @driver.find_element(:name, 'usr_password')
 usr_pass.send_keys(@usr_password)
 @driver.find_element(:name, 'OK').click
 
-sleep 2
+sleep 3
 
 def find_element_by_xpath(path)
   @driver.find_element(:xpath, path)
