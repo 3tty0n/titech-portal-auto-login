a6d49f0c769bfc6641ebca1897c30eb30781f29e
Yusuke Izawa <yuizalp@gmail.com>
2017-05-06 22:38:39 +0900

    update login script and remove unused file

--- titech_portal_auto_login.rb
+++ titech_portal_auto_login.rb
@@ -1,5 +1,10 @@
+require 'pit'
 require 'selenium-webdriver'
-require './restore.rb'
+
+config = Pit.get('titech')
+@usr_name = config['usr_name']
+@usr_password = config['usr_password']
+@usr_matrix = Hash[*[('A'..'J').to_a, config['usr_matrix']].transpose.flatten]
 
 @driver = Selenium::WebDriver.for :firefox
 
